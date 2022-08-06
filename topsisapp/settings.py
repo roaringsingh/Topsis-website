4571c2f23c98fc40e5a6dfc6ac1e92e1d98236be
roaringsingh <prabhnoorsingh2610@gmail.com>
2022-08-06 12:51:36 +0530

    env var for database

--- topsisapp/settings.py
+++ topsisapp/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'topsisapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv(DB_NAME),
-        'USER': os.getenv(DB_USER),
-        'PASSWORD': os.getenv(DB_PASSWORD),
-        'HOST': os.getenv(DB_HOST),
-        'PORT':  int(os.getenv(DB_PORT)),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT':  int(os.getenv('DB_PORT')),
     }
 }
 
